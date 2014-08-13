abacd93b7d2a6eede6e2d34571e5b7d59e4d7155
Martin Hansson <sylle90@hotmail.com>
2014-08-13 20:40:22 +0200

    Removed a few comments.

--- public/js/controllers/LoginCtrl.js
+++ public/js/controllers/LoginCtrl.js
@@ -1,20 +1,16 @@
 app.controller('LoginCtrl', function($scope, $rootScope, $http, $location) {
-  // This object will be filled by the form
   $scope.user = {};
 
-  // Register the login() function
   $scope.login = function(){
     $http.post('/Login', {
       username: $scope.user.username,
       password: $scope.user.password,
     })
     .success(function(user){
-      // No error: authentication OK
       $rootScope.message = 'Authentication successful!';
       $location.url('/Blog');
     })
     .error(function(){
-      // Error: authentication failed
       $rootScope.message = 'Authentication failed.';
       $location.url('/Login');
     });
