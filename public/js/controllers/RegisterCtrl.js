5cb43de7891c63a2d95673f2a5b338f45766582d
cernael <tomas@cernael.com>
2014-08-17 19:43:21 +0200

    indentation

--- public/js/controllers/RegisterCtrl.js
+++ public/js/controllers/RegisterCtrl.js
@@ -1,9 +1,9 @@
 app.controller("RegisterCtrl", function($scope, $routeParams, $location, User) {
 
-$scope.submit = function(){
-  User.create($scope.UserData);
-  console.log($scope.UserData);
-};
+  $scope.submit = function(){
+    User.create($scope.UserData);
+    console.log($scope.UserData);
+  };
 
 
 });
